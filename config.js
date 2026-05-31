a33d48bb17a134e9337c3519c06a3ec990bc32f7
ilkerdijital-oss <ilker.dijital@gmail.com>
2026-05-31 03:32:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
   url: "https://jtloypulmmmvybhvovgv.supabase.co",
-  anonKey: "https://jtloypulmmmvybhvovgv.supabase.co/rest/v1/"
+  anonKey: "sb_publishable_U_Bc6ijw7yD_azO0E_j4YQ_P-kHDHJh"
 };
